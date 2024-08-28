a9d679ac754de7abec3b6c8bee7bb08a63813267
Elias Pölönen <elias.polonen@edu.riveria.fi>
2024-08-28 08:27:24 +0300

    2.4

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ import { usersRoutes } from "./routes/api.js";
 
 const app = express();
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json({ type: 'application/json' }))
@@ -31,4 +31,5 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`user-management-app listening on port ${port}`);
+  console.log(`http://localhost:${port}`);
 });
